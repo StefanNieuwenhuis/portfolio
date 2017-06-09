7d9952a3d9df5de29f630e0c5c4127a8b197050e
Stefan Nieuwenhuis <stefan.nieuwenhuis@ordina.nl>
2017-06-09 09:39:25 +0200

    Change home
    Update test
    Update layout

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home.component';
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let content: string = "home works!";
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,5 +22,15 @@ describe('HomeComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should have as title 'home works!'`, async () => {
+    const home = fixture.debugElement.componentInstance;
+    expect(home.title).toEqual(content);
+  });
+
+  it('should render title in a p tag', async () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(content);
   });
 });
